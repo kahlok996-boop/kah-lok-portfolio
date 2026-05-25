ca0a2df6d95b753f831f65a63ba81e86e8a8ea63
Kah Lok <kahlok996@gmail.com>
2026-05-26 02:47:20 +0800

    Fix popup url

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
   const [showPopup, setShowPopup] = useState(false);
 
   const auditFormUrl =
-    "https://kahlokstudio.comhttps://kahlokstudio.com";
+    "https://kahlokstudio.com";
 
   useEffect(() => {
     const timer = setTimeout(() => {
